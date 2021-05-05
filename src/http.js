38b9e2dae541169ef0ea88020c2e6f4204b524c8
Zachary M <31627113+zmontgo@users.noreply.github.com>
2021-05-05 11:18:03 -0500

    Update http.js

--- src/http.js
+++ src/http.js
@@ -3,31 +3,35 @@ const config = require('../config.json');
 
 module.exports = async function req(token, method, endpoint, data=null, body=null) {
   var url = config.baseURL + endpoint;
+  
+  var response;
+  var headers = {
+    Authorization: 'Bearer ' + token
+  };
+  var body = {};
 
-  if (typeof(Http[method]) === 'function') return Http[method](token, url, data, body);
+  if (data) {
+    if (data.revision) headers['Wanikani-Revision'] = data.revision;
+    if (data.modified) headers['If-Modified-Since'] = data.modified;
+    if (data.nonematch) headers['If-None-Match'] = data.nonematch;
+
+    if (data.filter) {
+      url = url + '?';
+      for (const key in data.filter) {
+        url = url + key + '=' + data.filter[key] + '&';
+      }
+    }
+    if (data.body) {
+      body[body] = data.body;
+    }
+  }
+
+  if (typeof(Http[method]) === 'function') return Http[method](token, url, headers, body);
   else return false;
 }
 
 class Http {
-  static async get(token, url, data) {
-    var response;
-    var headers = {
-      Authorization: 'Bearer ' + token
-    };
-
-    if (data) {
-      if (data.revision) headers['Wanikani-Revision'] = data.revision;
-      if (data.modified) headers['If-Modified-Since'] = data.modified;
-      if (data.nonematch) headers['If-None-Match'] = data.nonematch;
-      
-      if (data.filter) {
-        url = url + '?';
-        for (const key in data.filter) {
-          url = url + key + '=' + data.filter[key] + '&';
-        }
-      }
-    }
-
+  static async get(token, url, headers) {
     try {
       response = await axios.get(url, { headers: headers });
     } catch(err) {
@@ -41,31 +45,15 @@ class Http {
     return response.data;
   }
 
-  static async post(token, url, data, body) {
-    var response;
-    var headers = {
-      Authorization: 'Bearer ' + token
-    };
-    var body = {};
-
-    if (data) {
-      if (data.revision) headers['Wanikani-Revision'] = data.revision;
-      if (data.modified) headers['If-Modified-Since'] = data.modified;
-      if (data.nonematch) headers['If-None-Match'] = data.nonematch;
-
-      if (data.filter) {
-        url = url + '?';
-        for (const key in data.filter) {
-          url = url + key + '=' + data.filter[key] + '&';
-        }
-      }
-      if (data.body) {
-        body[body] = data.body;
-      }
-    }
-
+  static async post(token, url, headers, body) {
     try {
-      response = await axios.post(url, { headers: headers });
+      response = await axios.post(
+        url,
+        {
+          headers: headers,
+          body: body
+        }
+      );
     } catch(err) {
       if(!err.response) return err;
 
@@ -78,30 +66,14 @@ class Http {
   }
   
   static async put(token, url, data, body) {
-    var response;
-    var headers = {
-      Authorization: 'Bearer ' + token
-    };
-    var body = {};
-
-    if (data) {
-      if (data.revision) headers['Wanikani-Revision'] = data.revision;
-      if (data.modified) headers['If-Modified-Since'] = data.modified;
-      if (data.nonematch) headers['If-None-Match'] = data.nonematch;
-
-      if (data.filter) {
-        url = url + '?';
-        for (const key in data.filter) {
-          url = url + key + '=' + data.filter[key] + '&';
-        }
-      }
-      if (data.body) {
-        body[body] = data.body;
-      }
-    }
-
     try {
-      response = await axios.put(url, { headers: headers });
+      response = await axios.put(
+        url,
+        {
+          headers: headers,
+          body: body
+        }
+      );
     } catch(err) {
       if(!err.response) return err;
 
