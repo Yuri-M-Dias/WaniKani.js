b6203b2b2c8deed0a0172742e92ef73207c12f96
Zachary Montgomery <31627113+zmontgo@users.noreply.github.com>
2021-05-05 17:52:59 -0500

    Update bugs with update_user

--- src/http.js
+++ src/http.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const config = require('../config.json');
 
-module.exports = async function req(token, method, endpoint, data=null, body=null) {
+module.exports = async function req(token, method, endpoint, data=null, body_value=null) {
   var url = config.baseURL + endpoint;
   
   var headers = {
@@ -21,76 +21,32 @@ module.exports = async function req(token, method, endpoint, data=null, body=nul
       }
     }
     if (data.body) {
-      if (body) {
-        body[body] = data.body;
+      if (body_value) {
+        body[body_value] = data.body;
       } else {
         body = data.body;
       }
     }
   }
 
-  if (typeof(Http[method]) === 'function') return Http[method](url, headers, body);
-  else return false;
-}
-
-class Http {
-  static async get(url, headers) {
-    var response;
-
-    try {
-      response = await axios.get(url, { headers: headers });
-    } catch(err) {
-      if(!err.response) return err;
-
-      return err.response.data;
-    }
-
-    if(!response) return false;
-
-    return response.data;
-  }
-
-  static async post(url, headers, body) {
   var response;
 
-    try {
-      response = await axios.post(
-        url,
-        {
-          headers: headers,
-          body: body
-        }
-      );
-    } catch(err) {
-      if(!err.response) return err;
+  console.log(body)
 
-      return err.response.data;
-    }
+  try {
+    response = await axios({
+      method: method,
+      url: url,
+      headers: headers,
+      data: body
+    });
+  } catch(err) {
+    if(!err.response) return err;
 
-    if(!response) return false;
-
-    return response.data;
+    return err.response.data;
   }
-  
-  static async put(url, headers, body) {
-    var response;
-    
-    try {
-      response = await axios.put(
-        url,
-        {
-          headers: headers,
-          body: body
-        }
-      );
-    } catch(err) {
-      if(!err.response) return err;
 
-      return err.response.data;
-    }
+  if(!response) return false;
 
-    if(!response) return false;
-
-    return response.data;
-  }
+  return response.data;
 }
--- src/member.js
+++ src/member.js
@@ -2,36 +2,35 @@ const endpoints = require('./endpoints');
 
 module.exports = class Member {
   constructor (token) {
-    const endpoint = new endpoints(token);
-
     this.token = token;
     this.timestamp = new Date();
-    this.user = endpoint.user();
-    this.lessons = endpoint.progressions();
-    this.resets = endpoint.resets();
-    this.reviews = endpoint.reviews();
-    this.review_statistics = endpoint.review_stats();
-    this.study_materials = endpoint.study_materials();
-    this.summary = endpoint.summary();
+    this.endpoint = new endpoints(token);
+    this.user = this.endpoint.user();
+    this.lessons = this.endpoint.progressions();
+    this.resets = this.endpoint.resets();
+    this.reviews = this.endpoint.reviews();
+    this.review_statistics = this.endpoint.review_stats();
+    this.study_materials = this.endpoint.study_materials();
+    this.summary = this.endpoint.summary();
   }
   
   async update_user(data) {
-    return await endpoint.update_user(data);
+    return await this.endpoint.update_user(data);
   }
 
   async start_assignment() {
-    return await endpoint.start_assignment(data);
+    return await this.endpoint.start_assignment(data);
   }
 
   async create_review(data) {
-    return await endpoint.create_review(data);
+    return await this.endpoint.create_review(data);
   }
 
   async create_study_material(data) {
-    return await endpoint.create_study_material(data);
+    return await this.endpoint.create_study_material(data);
   }
 
   async update_study_material() {
-    return await endpoint.update_study_material(data);
+    return await this.endpoint.update_study_material(data);
   }
 }
