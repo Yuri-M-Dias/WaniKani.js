10901243121db139d6ada1d6ea34ef9966f00a83
Zachary M <31627113+zmontgo@users.noreply.github.com>
2021-05-05 11:05:11 -0500

    Update http.js

--- src/http.js
+++ src/http.js
@@ -1,10 +1,10 @@
 const axios = require('axios');
 const config = require('../config.json');
 
-module.exports = async function req(token, method, endpoint, data=null) {
+module.exports = async function req(token, method, endpoint, data=null, body=null) {
   var url = config.baseURL + endpoint;
 
-  if (typeof(Http[method]) === 'function') return Http[method](token, url, data);
+  if (typeof(Http[method]) === 'function') return Http[method](token, url, data, body);
   else return false;
 }
 
@@ -41,7 +41,7 @@ class Http {
     return response.data;
   }
 
-  static async put(token, url, data) {
+  static async post(token, url, data, body) {
     var response;
     var headers = {
       Authorization: 'Bearer ' + token
@@ -60,12 +60,12 @@ class Http {
         }
       }
       if (data.body) {
-        body[config.endpoints.create_review.body] = data.body;
+        body[body] = data.body;
       }
     }
 
     try {
-      response = await axios.get(url, { headers: headers });
+      response = await axios.post(url, { headers: headers });
     } catch(err) {
       if(!err.response) return err;
 
@@ -77,7 +77,7 @@ class Http {
     return response.data;
   }
   
-  static async post(token, url, data) {
+  static async put(token, url, data, body) {
     var response;
     var headers = {
       Authorization: 'Bearer ' + token
@@ -96,7 +96,7 @@ class Http {
         }
       }
       if (data.body) {
-        body[config.endpoints.create_review.body] = data.body;
+        body[body] = data.body;
       }
     }
 
